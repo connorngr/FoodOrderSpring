f55cc0c9e5ea751314cdc1de1fc6f45730384a6c
Connor Nguyen <78084702+connorngr@users.noreply.github.com>
2024-07-01 15:23:30 +0700

    90%

--- src/main/java/com/SaiGonEats/SaiGonEats/SecurityConfig.java
+++ src/main/java/com/SaiGonEats/SaiGonEats/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationEntryPoint;
 
+
 @Configuration // Đánh dấu lớp này là một lớp cấu hình cho Spring Context.
 @EnableWebSecurity // Kích hoạt tính năng bảo mật web của Spring Security.
 @RequiredArgsConstructor // Lombok tự động tạo constructor có tham số cho tất cả các trường final.
@@ -45,8 +46,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(@NotNull HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/images/**", "/items/view/**", "/js/**", "/", "/oauth/**", "/register", "/error",
-                                "/products")//, "/cart", "/cart/**"
+                        .requestMatchers("/css/**", "/images/**", "/image/**", "/items/view/**", "/js/**", "/", "/oauth/**", "/register", "/error",
+                                "/products")
                         .permitAll() // Cho phép truy cập không cần xác thực.
                         .requestMatchers("/users", "/products/edit/**", "/products/add", "/products/delete")
                         .hasAnyAuthority("ADMIN") // Chỉ cho phép ADMIN truy cập.
